960283e74eac5d73b25512428d84b174267bc1c0
fluidvanadium <fluidvanadium@zingolabs.org>
2024-10-28 20:52:20 +0000

    init provider in all live tests

--- zingolib/src/lightclient/send.rs
+++ zingolib/src/lightclient/send.rs
@@ -366,6 +366,11 @@ pub mod send_with_proposal {
         /// this is a live send test. whether it can work depends on the state of live wallet on the blockchain
         /// this wallet contains archaic diversified addresses, which may clog the new send engine.
         async fn testnet_mskmgdbhotbpetcjwcspgopp_shield_multi_account() {
+            // install default crypto provider (ring)
+            if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+                log::error!("Error installing crypto provider: {:?}", e)
+            };
+
             let case =
                 ExampleWalletNetwork::Testnet(ExampleTestnetWalletSeed::MSKMGDBHOTBPETCJWCSPGOPP(
                     ExampleMSKMGDBHOTBPETCJWCSPGOPPVersion::Ga74fed621,
@@ -383,6 +388,11 @@ pub mod send_with_proposal {
         /// this is a live send test. whether it can work depends on the state of live wallet on the blockchain
         /// note: live send waits 2 minutes for confirmation. expect 3min runtime
         async fn testnet_cbbhrwiilgbrababsshsmtpr_send_to_self_orchard_hot() {
+            // install default crypto provider (ring)
+            if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+                log::error!("Error installing crypto provider: {:?}", e)
+            };
+
             let case =
                 ExampleWalletNetwork::Testnet(ExampleTestnetWalletSeed::CBBHRWIILGBRABABSSHSMTPR(
                     ExampleCBBHRWIILGBRABABSSHSMTPRVersion::G2f3830058,
@@ -409,6 +419,11 @@ pub mod send_with_proposal {
         /// this is a live sync test. its execution time scales linearly since last updated
         /// note: live send waits 2 minutes for confirmation. expect 3min runtime
         async fn testnet_cbbhrwiilgbrababsshsmtpr_shield_hot() {
+            // install default crypto provider (ring)
+            if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+                log::error!("Error installing crypto provider: {:?}", e)
+            };
+
             let case =
                 ExampleWalletNetwork::Testnet(ExampleTestnetWalletSeed::CBBHRWIILGBRABABSSHSMTPR(
                     ExampleCBBHRWIILGBRABABSSHSMTPRVersion::G2f3830058,
@@ -461,6 +476,11 @@ pub mod send_with_proposal {
         /// note: live send waits 2 minutes for confirmation. expect 3min runtime
         #[ignore = "dont automatically run hot tests! this test spends actual zec!"]
         async fn mainnet_hhcclaltpcckcsslpcnetblr_send_to_self_sapling_hot() {
+            // install default crypto provider (ring)
+            if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+                log::error!("Error installing crypto provider: {:?}", e)
+            };
+
             let case = ExampleWalletNetwork::Mainnet(crate::wallet::disk::testing::examples::ExampleMainnetWalletSeed::HHCCLALTPCCKCSSLPCNETBLR(crate::wallet::disk::testing::examples::ExampleHHCCLALTPCCKCSSLPCNETBLRVersion::Latest));
             let target_pool = PoolType::Shielded(ShieldedProtocol::Sapling);
 
@@ -492,6 +512,11 @@ pub mod send_with_proposal {
         /// note: live send waits 2 minutes for confirmation. expect 3min runtime
         #[ignore = "dont automatically run hot tests! this test spends actual zec!"]
         async fn mainnet_hhcclaltpcckcsslpcnetblr_send_to_self_transparent_hot() {
+            // install default crypto provider (ring)
+            if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+                log::error!("Error installing crypto provider: {:?}", e)
+            };
+
             let case = ExampleWalletNetwork::Mainnet(crate::wallet::disk::testing::examples::ExampleMainnetWalletSeed::HHCCLALTPCCKCSSLPCNETBLR(crate::wallet::disk::testing::examples::ExampleHHCCLALTPCCKCSSLPCNETBLRVersion::Latest));
             let target_pool = PoolType::Transparent;
 
@@ -523,6 +548,11 @@ pub mod send_with_proposal {
         /// note: live send waits 2 minutes for confirmation. expect 3min runtime
         #[ignore = "dont automatically run hot tests! this test spends actual zec!"]
         async fn mainnet_hhcclaltpcckcsslpcnetblr_shield_hot() {
+            // install default crypto provider (ring)
+            if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
+                log::error!("Error installing crypto provider: {:?}", e)
+            };
+
             let case = ExampleWalletNetwork::Mainnet(crate::wallet::disk::testing::examples::ExampleMainnetWalletSeed::HHCCLALTPCCKCSSLPCNETBLR(crate::wallet::disk::testing::examples::ExampleHHCCLALTPCCKCSSLPCNETBLRVersion::Latest));
             let client = sync_example_wallet(case).await;
 
