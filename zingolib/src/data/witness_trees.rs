c4f96a49afd008b37993a0f74202ba4577321cde
fluidvanadium <fluidvanadium@zingolabs.org>
2024-10-30 21:34:37 +0000

    truncate_to_checkpoint

--- zingolib/src/data/witness_trees.rs
+++ zingolib/src/data/witness_trees.rs
@@ -157,12 +157,11 @@ impl WitnessTrees {
     /// helper truncates shard trees for both shielded pools
     pub(crate) fn truncate_to_checkpoint(&mut self, checkpoint_height: BlockHeight) {
         self.witness_tree_sapling
-            .truncate_removing_checkpoint(&checkpoint_height)
+            .truncate_to_checkpoint(&checkpoint_height)
             .expect("Infallible");
         self.witness_tree_orchard
-            .truncate_removing_checkpoint(&checkpoint_height)
+            .truncate_to_checkpoint(&checkpoint_height)
             .expect("Infallible");
-        self.add_checkpoint(checkpoint_height);
     }
 
     const VERSION: u8 = 0;
