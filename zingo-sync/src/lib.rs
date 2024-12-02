c96ae40da2453a10fe9605e67a37fc14ec10d673
zancas <zancas@zingolabs.org>
2024-12-02 09:17:40 -0700

    first draft of basic definitions

--- zingo-sync/src/lib.rs
+++ zingo-sync/src/lib.rs
@@ -1,6 +1,12 @@
 #![warn(missing_docs)]
 //! Zingo sync engine prototype
 //!
+//! Definitions:
+//!  Sync: Observation of a consensus state
+//!  Consensus State:  Eventually consistent global agreement
+//!  Key-Enabled (keyed): Sync where the observer owns keys that reveal hidden information
+//!  Keyless: The observer doesn't have keys
+//!
 //! Entrypoint: [`crate::sync::sync`]
 
 pub mod client;
