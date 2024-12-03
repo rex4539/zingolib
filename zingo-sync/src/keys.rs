2fc0482d8ce8b041074075b80cd48bfbaef57c74
Oscar Pepper <oscarpepper@zingolabs.org>
2024-12-03 02:02:43 +0000

    fix doc warnings

--- zingo-sync/src/keys.rs
+++ zingo-sync/src/keys.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use getset::Getters;
 use incrementalmerkletree::Position;
 use orchard::{
-    keys::{FullViewingKey, IncomingViewingKey, Scope},
+    keys::{FullViewingKey, IncomingViewingKey},
     note_encryption::OrchardDomain,
 };
 use sapling_crypto::{
@@ -14,6 +14,7 @@ use sapling_crypto::{
 use zcash_keys::keys::UnifiedFullViewingKey;
 use zcash_note_encryption::Domain;
 use zcash_primitives::zip32::AccountId;
+use zip32::Scope;
 
 /// Child index for the `address_index` path level in the BIP44 hierarchy.
 pub type AddressIndex = u32;
@@ -90,7 +91,7 @@ pub enum TransparentScope {
 }
 
 /// A key that can be used to perform trial decryption and nullifier
-/// computation for a [`CompactSaplingOutput`] or [`CompactOrchardAction`].
+/// computation for a CompactSaplingOutput or CompactOrchardAction.
 pub trait ScanningKeyOps<D: Domain, Nf> {
     /// Prepare the key for use in batch trial decryption.
     fn prepare(&self) -> D::IncomingViewingKey;
