133c79383043247f03f6964449749f1a536c2ba1
fluidvanadium <fluidvanadium@zingolabs.org>
2024-12-05 21:49:08 +0000

    check mempool in local libtonode

--- libtonode-tests/tests/chain_generics.rs
+++ libtonode-tests/tests/chain_generics.rs
@@ -8,9 +8,9 @@ proptest::proptest! {
     #![proptest_config(proptest::test_runner::Config::with_cases(8))]
     #[test]
     fn single_sufficient_send(send_value in 0..50_000u64, change_value in 0..10_000u64, sender_protocol in 1..2, receiver_pool in 1..2) {
-        // note: this darkside test does not check the mempool
+        // note: mempool check is hereby disabled in CI
         Runtime::new().unwrap().block_on(async {
-            fixtures::single_sufficient_send::<LibtonodeEnvironment>(int_to_shieldedprotocol(sender_protocol), int_to_pooltype(receiver_pool), send_value, change_value, false).await;
+            fixtures::single_sufficient_send::<LibtonodeEnvironment>(int_to_shieldedprotocol(sender_protocol), int_to_pooltype(receiver_pool), send_value, change_value, !cfg!(feature = "ci")).await;
         });
      }
 }
