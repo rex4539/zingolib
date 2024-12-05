56c48c0d18f1fb9e0a055d8d13a9e744d78e2fee
fluidvanadium <fluidvanadium@zingolabs.org>
2024-12-05 23:00:32 +0000

    helperized follow proposal

--- zingolib/src/testutils/chain_generics/with_assertions.rs
+++ zingolib/src/testutils/chain_generics/with_assertions.rs
@@ -7,8 +7,11 @@ use crate::testutils::chain_generics::conduct_chain::ConductChain;
 use crate::testutils::lightclient::from_inputs;
 use crate::testutils::lightclient::get_base_address;
 use crate::testutils::lightclient::lookup_statuses;
+use nonempty::NonEmpty;
+use zcash_client_backend::proposal::Proposal;
 use zcash_client_backend::PoolType;
 use zcash_primitives::consensus::BlockHeight;
+use zcash_primitives::transaction::TxId;
 use zingo_status::confirmation_status::ConfirmationStatus;
 
 /// this function handles inputs and their lifetimes to create a proposal
@@ -49,19 +52,68 @@ where
     CC: ConductChain,
 {
     sender.do_sync(false).await.unwrap();
-    let server_height_at_send = BlockHeight::from(
-        crate::grpc_connector::get_latest_block(environment.lightserver_uri().unwrap())
-            .await
-            .unwrap()
-            .height as u32,
-    );
-
     let proposal = to_clients_proposal(sender, &sends).await;
 
     let txids = sender
         .complete_and_broadcast_stored_proposal()
         .await
         .unwrap();
+
+    follow_proposal(
+        environment,
+        sender,
+        sends
+            .iter()
+            .map(|(recipient, _, _, _)| *recipient)
+            .collect(),
+        &proposal,
+        txids,
+        test_mempool,
+    )
+    .await
+    .unwrap()
+}
+
+/// a test-only generic version of shield that includes assertions that the proposal was fulfilled
+/// NOTICE this function bumps the chain and syncs the client
+/// only compatible with zip317
+/// returns the total fee for the transfer
+pub async fn assure_propose_shield_bump_sync<CC>(
+    environment: &mut CC,
+    client: &LightClient,
+    test_mempool: bool,
+) -> Result<u64, String>
+where
+    CC: ConductChain,
+{
+    let proposal = client.propose_shield().await.map_err(|e| e.to_string())?;
+
+    let txids = client
+        .complete_and_broadcast_stored_proposal()
+        .await
+        .unwrap();
+
+    follow_proposal(environment, sender, vec![], &proposal, txids, test_mempool).await
+}
+
+/// given a just-broadcast proposal, confirms that it achieves all expected checkpoints.
+pub async fn follow_proposal<CC, NoteRef>(
+    environment: &mut CC,
+    sender: &LightClient,
+    recipients: Vec<&LightClient>,
+    proposal: &Proposal<zcash_primitives::transaction::fees::zip317::FeeRule, NoteRef>,
+    txids: NonEmpty<TxId>,
+    test_mempool: bool,
+) -> Result<u64, String>
+where
+    CC: ConductChain,
+{
+    let server_height_at_send = BlockHeight::from(
+        crate::grpc_connector::get_latest_block(environment.lightserver_uri().unwrap())
+            .await
+            .unwrap()
+            .height as u32,
+    );
 
     // digesting the calculated transaction
     // this step happens after transaction is recorded locally, but before learning anything about whether the server accepted it
@@ -101,8 +153,7 @@ where
             )
         });
 
-        // TODO: distribute receivers
-        for (recipient, _, _, _) in sends.clone() {
+        for recipient in recipients.clone() {
             recipient.do_sync(false).await.unwrap();
 
             lookup_statuses(recipient, txids.clone())
@@ -130,7 +181,7 @@ where
         assert!(matches!(status, Some(ConfirmationStatus::Confirmed(_))));
     });
 
-    for (recipient, _, _, _) in sends {
+    for recipient in recipients {
         recipient.do_sync(false).await.unwrap();
         lookup_statuses(recipient, txids.clone())
             .await
@@ -141,78 +192,6 @@ where
                 )
             });
     }
-    recorded_fee
-}
-
-/// a test-only generic version of shield that includes assertions that the proposal was fulfilled
-/// NOTICE this function bumps the chain and syncs the client
-/// only compatible with zip317
-/// returns the total fee for the transfer
-pub async fn assure_propose_shield_bump_sync<CC>(
-    environment: &mut CC,
-    client: &LightClient,
-    test_mempool: bool,
-) -> Result<u64, String>
-where
-    CC: ConductChain,
-{
-    let proposal = client.propose_shield().await.map_err(|e| e.to_string())?;
-
-    let server_height_at_send = BlockHeight::from(
-        crate::grpc_connector::get_latest_block(environment.lightserver_uri().unwrap())
-            .await
-            .unwrap()
-            .height as u32,
-    );
-
-    let txids = client
-        .complete_and_broadcast_stored_proposal()
-        .await
-        .unwrap();
-
-    // digesting the calculated transaction
-    let recorded_fee = *lookup_fees_with_proposal_check(client, &proposal, &txids)
-        .await
-        .first()
-        .expect("one transaction proposed")
-        .as_ref()
-        .expect("record is ok");
-
-    lookup_statuses(client, txids.clone()).await.map(|status| {
-        assert_eq!(
-            status,
-            Some(ConfirmationStatus::Transmitted(server_height_at_send))
-        );
-    });
-
-    if test_mempool {
-        // mempool scan shows the same
-        client.do_sync(false).await.unwrap();
-        lookup_fees_with_proposal_check(client, &proposal, &txids)
-            .await
-            .first()
-            .expect("one transaction proposed")
-            .as_ref()
-            .expect("record is ok");
-
-        lookup_statuses(client, txids.clone()).await.map(|status| {
-            assert!(matches!(status, Some(ConfirmationStatus::Mempool(_))));
-        });
-    }
-
-    environment.bump_chain().await;
-    // chain scan shows the same
-    client.do_sync(false).await.unwrap();
-    lookup_fees_with_proposal_check(client, &proposal, &txids)
-        .await
-        .first()
-        .expect("one transaction proposed")
-        .as_ref()
-        .expect("record is ok");
-
-    lookup_statuses(client, txids.clone()).await.map(|status| {
-        assert!(matches!(status, Some(ConfirmationStatus::Confirmed(_))));
-    });
 
     Ok(recorded_fee)
 }
