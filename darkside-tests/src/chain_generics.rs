6700069432799ec350b42fc6ee20a122ae292bb3
fluidvanadium <fluidvanadium@zingolabs.org>
2024-12-07 01:06:18 +0000

    turn off darkside mempool

--- darkside-tests/src/chain_generics.rs
+++ darkside-tests/src/chain_generics.rs
@@ -26,7 +26,7 @@ proptest! {
     #[test]
     fn single_sufficient_send_0_change_darkside(send_value in 0..50_000u64, sender_protocol in 1..2, receiver_pool in 0..2) {
         Runtime::new().unwrap().block_on(async {
-            fixtures::single_sufficient_send::<DarksideEnvironment>(int_to_shieldedprotocol(sender_protocol), int_to_pooltype(receiver_pool), send_value, 0, true).await;
+            fixtures::single_sufficient_send::<DarksideEnvironment>(int_to_shieldedprotocol(sender_protocol), int_to_pooltype(receiver_pool), send_value, 0, false).await;
         });
      }
 }
